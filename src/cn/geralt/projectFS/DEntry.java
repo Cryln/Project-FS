7cf18fefd8a91cc7ae26228ae54f2205c7840e01
Geralt <superj0529@gmail.com>
2021-05-28 16:03:13 +0800

    dentry

--- src/cn/geralt/projectFS/DEntry.java
+++ src/cn/geralt/projectFS/DEntry.java
@@ -1,4 +1,24 @@
 package cn.geralt.projectFS;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class DEntry {
+    private DEntry parent;
+    private ArrayList<DEntry> children;
+    private int iNodeNum;
+    private INode iNode;
+    private byte[] rawFile;
+
+    public DEntry(DEntry parent, int iNodeNum) {
+        if(parent==null){
+            this.parent = this;
+        }else
+            this.parent = parent;
+        this.iNodeNum = iNodeNum;
+    }
+    public void open() throws IOException {
+        this.iNode = new INode(this.iNodeNum);
+        rawFile = iNode.getFile();
+    }
 }
