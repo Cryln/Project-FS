cd79d0c2a38383719cd6385082ddcfd8dc6c64d9
Geralt <superj0529@gmail.com>
2021-06-01 15:21:30 +0800

    fix some version conflict

--- src/cn/geralt/util/ByteIO.java
+++ src/cn/geralt/util/ByteIO.java
@@ -1,9 +1,11 @@
 package cn.geralt.util;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class ByteIO {
     private String filedir;
+    private int pos;
 //    private File file;
 //    private FileOutputStream fileOutputStream;
 //    private FileInputStream fileInputStream;
@@ -14,7 +16,13 @@ public class ByteIO {
 //        this.fileInputStream = new FileInputStream(this.file);
 //        this.fileOutputStream = new FileOutputStream(this.file);
         this.randomAccessFile = new RandomAccessFile(this.filedir,"rw");
+        pos = 0;
     }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
     public void input(String content, int off) throws IOException {
         byte[] bytes =  content.getBytes();
 //        this.fileOutputStream.write(bytes,off,len);
@@ -36,22 +44,49 @@ public class ByteIO {
         this.randomAccessFile.readByte();
         return bytes;
     }
-
-    public static void main(String[] args) throws IOException {
-        ByteIO byteIO = new ByteIO("src/cn/geralt/util/mydisk.vhd");
-//        ByteIO byteIO2 = new ByteIO("D:\\Codes\\java\\Project-FS\\src\\cn\\geralt\\util\\JOS.vhd");
-        byteIO.input("hello world",4096);
-        byte[] bytes = byteIO.output(4096,11);
-        for (byte aByte : bytes) {
-            System.out.println(String.format("%X",aByte));
-        }
-        System.out.println();
-        System.out.println(new String(bytes));
+    public int nextInt() throws IOException {
+        byte[] data = output(pos,4);
+        pos += 4;
+        return byteArrayToInt(data);
     }
+
+    public byte nextByte() throws IOException {
+        byte[] data = output(pos,1);
+        pos ++;
+        return data[0];
+    }
+
+    public void writeInt(int a) throws IOException {
+        byte[] data = intToByteArray(a);
+        input(data,pos);
+        pos += 4;
+    }
+    public void writeBytes(byte[] a) throws IOException {
+        input(a,pos);
+        pos += a.length;
+    }
+
     public static int byteArrayToInt(byte[] b) {
         return   b[3] & 0xFF |
                 (b[2] & 0xFF) << 8 |
                 (b[1] & 0xFF) << 16 |
                 (b[0] & 0xFF) << 24;
+    }
+
+    public static byte[] intToByteArray(int n) {
+        byte[] b = new byte[4];
+        b[3] = (byte) (n & 0xff);
+        b[2] = (byte) (n >> 8 & 0xff);
+        b[1] = (byte) (n >> 16 & 0xff);
+        b[0] = (byte) (n >> 24 & 0xff);
+        return b;
+    }
+
+    public static int byteArrayToInt(byte[] b,int off){
+        return byteArrayToInt(Arrays.copyOfRange(b,off,off+4));
+    }
+
+    public static void main(String[] args) throws IOException {
+
     }
 }
