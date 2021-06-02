c5d1cf725de6eceb328028dae208a44513dfe1ef
Geralt <superj0529@gmail.com>
2021-06-02 11:18:21 +0800

    shell

--- src/cn/geralt/projectFS/Shell.java
+++ src/cn/geralt/projectFS/Shell.java
@@ -1,7 +1,14 @@
 package cn.geralt.projectFS;
 
+import cn.geralt.cmd.Executable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Shell {
     private FileSystem fileSystem;
+    private Scanner scanner = new Scanner(System.in);
 
     public FileSystem getFileSystem() {
         return fileSystem;
@@ -14,5 +21,15 @@ public class Shell {
         setFileSystem(fileSystem);
     }
 
+    public void run() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, ClassNotFoundException {
+        while (true){
+            String line = scanner.nextLine().strip();
+            String[] cmd = line.split("\\s+");
+            Class clazz = Class.forName("cn.geralt.cmd."+cmd[0]);
+            Executable exec = (Executable)clazz.getConstructor(FileSystem.class).newInstance(this.fileSystem);
+            exec.run(Arrays.copyOfRange(cmd,1,cmd.length));
+            System.out.println();
+        }
+    }
 
 }
